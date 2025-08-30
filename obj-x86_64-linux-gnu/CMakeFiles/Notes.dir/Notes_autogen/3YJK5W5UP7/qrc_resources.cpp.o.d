68fc978d8ec4d4505ee2428bca80873803418e32
Mustafa <mkafridi073@gmail.com>
2025-08-30 20:20:34 +0500

    Clean repository: remove all generated build files
    - Removed build/ and obj-x86_64-linux-gnu/ directories
    - Removed debian generated files (.debhelper/, files, logs, etc.)
    - Kept debian source files (control, rules, changelog, icons)
    - Updated .gitignore to prevent future build files from being tracked
    - Repository now contains only source code and documentation

--- obj-x86_64-linux-gnu/CMakeFiles/Notes.dir/Notes_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Notes.dir/Notes_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /media/mustafa/Data/Study/Projects/notes/obj-x86_64-linux-gnu/Notes_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
