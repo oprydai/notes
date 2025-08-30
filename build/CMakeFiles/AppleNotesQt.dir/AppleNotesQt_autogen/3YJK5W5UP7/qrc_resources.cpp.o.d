ebd0e29ad031d417d93e563713de23508223f37e
Mustafa <mkafridi073@gmail.com>
2025-08-30 20:18:31 +0500

    Remove build files from repository
    - Removed entire build/ directory and all generated files
    - Added .gitignore to prevent future build files from being tracked
    - Clean repository with only source code and documentation
    - Build files will be generated locally by users

--- build/CMakeFiles/AppleNotesQt.dir/AppleNotesQt_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/AppleNotesQt.dir/AppleNotesQt_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /media/mustafa/Data/Study/Projects/notes/build/AppleNotesQt_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Notes.dir/Notes_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Notes.dir/Notes_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /media/mustafa/Data/Study/Projects/notes/build/Notes_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
